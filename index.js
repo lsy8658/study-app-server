6032357c0d3ee93c38693e3bc7ae23d7f1854f3e
sungyoon <dltjddbs8658@naver.com>
2022-03-30 11:21:17 +0900

    index 수정

--- index.js
+++ index.js
@@ -28,10 +28,12 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 app.use(cookieParser());
-const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.json("안녕하세요 study-app server입니다.");
+});
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/study", studyRouter);
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("서버에 연결되었습니다.");
 });
