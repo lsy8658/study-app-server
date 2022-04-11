56a40e14100386883884d241426f247e366d6f06
sungyoon <dltjddbs8658@naver.com>
2022-04-11 21:15:18 +0900

    make it better

--- routers/auth.js
+++ routers/auth.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 // -------------------Token 생성-------------------
 const generateAccessToken = (user) => {
   return jwt.sign({ email: user.email }, process.env.ACCESS_TOKEN, {
-    expiresIn: "10s",
+    expiresIn: "2h",
     issuer: "sungyoon",
   });
 };
