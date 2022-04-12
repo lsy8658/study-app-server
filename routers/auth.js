e9dfaa437ffd1692e94b68abfb927fa6993bd207
sungyoon <dltjddbs8658@naver.com>
2022-04-12 15:50:56 +0900

    make it better

--- routers/auth.js
+++ routers/auth.js
@@ -12,7 +12,7 @@ const generateAccessToken = (user) => {
 };
 const generateRefreshToken = (user) => {
   return jwt.sign({ email: user.email }, process.env.REFRESH_TOKEN, {
-    expiresIn: "2d",
+    expiresIn: "4m",
     issuer: "sungyoon",
   });
 };
