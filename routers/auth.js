b9f7364e95a5117b8c727d46b8162276b46d43de
sungyoon <dltjddbs8658@naver.com>
2022-04-12 14:46:30 +0900

    make it better

--- routers/auth.js
+++ routers/auth.js
@@ -44,22 +44,22 @@ router.post("/refresh", async (req, res) => {
   const token = await user.token;
   try {
     // //토큰이 없거나 유효하지 않으면 오류를 보냅니다.
-    // !refreshToken && res.status(401).json("인증되지 않았습니다!");
-    // if (refreshTokens.includes(refreshToken)) {
-    //   return res.status(403).json("새로고침 토큰이 유효하지 않습니다!");
-    // }
+    !refreshToken && res.status(401).json("인증되지 않았습니다!");
+    if (refreshTokens.includes(refreshToken)) {
+      return res.status(403).json("새로고침 토큰이 유효하지 않습니다!");
+    }
     // // //모든 것이 정상이면 새 액세스 토큰을 생성하고 토큰을 새로고침하여 사용자에게 보냅니다.
-    // if (token === refreshToken) {
-    //   jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (error, user) => {
-    //     error && console.log(error);
+    if (token === refreshToken) {
+      jwt.verify(refreshToken, process.env.REFRESH_TOKEN, (error, user) => {
+        error && console.log(error);
 
-    //     const newAccessToken = generateAccessToken(user);
+        const newAccessToken = generateAccessToken(user);
 
-    return res.status(200).json(token);
-    //   });
-    // } else {
-    //   return res.status(403).json("새로고침 토큰이 유효하지 않습니다!");
-    // }
+        return res.status(200).json(newAccessToken);
+      });
+    } else {
+      return res.status(403).json("새로고침 토큰이 유효하지 않습니다!");
+    }
   } catch (err) {
     res.status(500).json(err);
   }
