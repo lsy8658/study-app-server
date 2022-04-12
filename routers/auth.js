bed9b9df5a132f741482e7c114a122f675e089fe
sungyoon <dltjddbs8658@naver.com>
2022-04-12 11:27:01 +0900

    업데이트

--- routers/auth.js
+++ routers/auth.js
@@ -6,7 +6,7 @@ const jwt = require("jsonwebtoken");
 // -------------------Token 생성-------------------
 const generateAccessToken = (user) => {
   return jwt.sign({ email: user.email }, process.env.ACCESS_TOKEN, {
-    expiresIn: "2h",
+    expiresIn: "20s",
     issuer: "sungyoon",
   });
 };
